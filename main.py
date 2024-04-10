cba28701415ee264bcdff8c8b8c737e0dfc276a8
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-10 16:01:06 -0600

    feat: add Claude AI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import google.generativeai as genai
 from IPython.display import Markdown
 import textwrap
+import anthropic
 
 
 def to_markdown(text):
@@ -17,8 +18,10 @@ class TicTacToe:
         self.current_winner = None  # keep track of winner!
 
     def print_board(self):
-        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
+        for i in range(3):
+            print('|'.join(self.board[i * 3:(i + 1) * 3]))
+            if i < 2:
+                print('-+-+-')
 
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == ' ']
@@ -58,6 +61,10 @@ class TicTacToe:
 
 
 def play(game, x_player, o_player, print_game=True):
+    # Print player names and their letters
+    print(f"{x_player.name} is '{x_player.letter}'")
+    print(f"{o_player.name} is '{o_player.letter}'")
+
     if print_game:
         game.print_board()
 
@@ -79,12 +86,12 @@ def play(game, x_player, o_player, print_game=True):
             if game.current_winner:
                 if print_game:
                     print(letter + ' wins!')
-                if isinstance(x_player, AIPlayer):
+                if isinstance(x_player, GeminiPlayer):
                     message = f"{letter} wins!"
                     print(f"Message to AI: {message}")
                     response = x_player.chat.send_message(message)
                     print(f"AI response: {response.text}")
-                if isinstance(o_player, AIPlayer):
+                if isinstance(o_player, GeminiPlayer):
                     message = f"{letter} wins!"
                     print(f"Message to AI: {message}")
                     response = o_player.chat.send_message(message)
@@ -92,22 +99,18 @@ def play(game, x_player, o_player, print_game=True):
                 return letter  # ends the loop and exits the game
             # after we made our move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'  # switches player
-            if isinstance(x_player, AIPlayer):
-                x_player.inform_move(square)
-            if isinstance(o_player, AIPlayer):
-                o_player.inform_move(square)
 
         # tiny break to make things a little easier to read
         time.sleep(0.8)
 
     if print_game:
         print('It\'s a tie!')
-        if isinstance(x_player, AIPlayer):
+        if isinstance(x_player, GeminiPlayer):
             message = "It's a tie!"
             print(f"Message to AI: {message}")
             response = x_player.chat.send_message(message)
             print(f"AI response: {response.text}")
-        if isinstance(o_player, AIPlayer):
+        if isinstance(o_player, GeminiPlayer):
             message = "It's a tie!"
             print(f"Message to AI: {message}")
             response = o_player.chat.send_message(message)
@@ -115,8 +118,9 @@ def play(game, x_player, o_player, print_game=True):
 
 
 class Player:
-    def __init__(self, letter):
+    def __init__(self, letter, name):
         self.letter = letter
+        self.name = name
 
     def get_move(self, game):
         valid_square = False
@@ -133,46 +137,87 @@ class Player:
         return val
 
 
-class AIPlayer(Player):
+class GeminiPlayer(Player):
     def __init__(self, letter, chat):
-        super().__init__(letter)
+        super().__init__(letter, 'Gemini')
         self.chat = chat
         message = "We're going to play Tic Tac Toe. You're '{}'. I'll tell you the position I put on the " \
                   "board. Please reply with your move as a digit from 1-9 only.".format(letter)
-        print(f"Message to AI: {message}")
-        response = self.chat.send_message(message)
-        print(f"AI response: {response.text}")
+        self.chat.send_message(message)
 
     def get_move(self, game):
         valid_square = False
         val = None
+        invalid_square = False
         while not valid_square:
-            message = f"{self.letter}'s turn. Please answer with a digit from 1-9 only."
-            print(f"Message to AI: {message}")
+            board_state = '\n'.join(['|'.join(game.board[i * 3:(i + 1) * 3]) for i in range(3)])
+            # Concatenate the board state and the message
+            if invalid_square:
+                message = (f"Invalid square. Try again.\n\nCurrent board state:\n{board_state}\n\n{self.letter}'s turn."
+                           f" Please answer with a digit from 1-9 only.")
+            else:
+                message = (f"Current board state:\n{board_state}\n\n{self.letter}'s turn. Please answer with a digit "
+                           f"from 1-9 only.")
             response = self.chat.send_message(message)
             square = response.text
-            print(f"AI response: {square}")
+            print(f"Gemini response: {square}")
             try:
                 val = int(square) - 1  # Subtract 1 here
                 if val not in game.available_moves():
                     raise ValueError
                 valid_square = True
             except ValueError:
-                print('Invalid square. Try again.')
+                print('Gemini: Invalid square. Try again.')
+                invalid_square = True
         return val
 
-    def inform_move(self, move):
-        message = f"I put my '{self.letter}' on position {move + 1}."
-        print(f"Message to AI: {message}")
-        self.chat.send_message(message)
+
+class ClaudePlayer(Player):
+    def __init__(self, letter, client):
+        super().__init__(letter, 'Claude')
+        self.client = client
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            game_state = ("The current game board is '{}'. What should be the next move for '{}'? "
+                          "Here is a important rule for game is answer only 1-9 digits. Only one digit is allowed."
+                          "Here is example <example>1</example>").format(''.join(game.board), self.letter)
+            message = self.client.messages.create(
+                model="claude-3-haiku-20240307",
+                system="You're the player of Tic Tac Toe game.",
+                max_tokens=1024,
+                messages=[
+                    {"role": "user", "content": game_state}
+                ]
+            )
+            print(f"Claude response: {message.content[0].text}")
+            square = message.content[0].text
+            try:
+                val = int(square) - 1  # Subtract 1 here
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Claude: Invalid square. Try again.')
+                print('Waiting for Claude rate limit to reset...')
+                time.sleep(10)
+        return val
 
 
 if __name__ == '__main__':
+    # Init Gemini AI
     genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat()
 
-    x_player = Player('X')
-    o_player = AIPlayer('O', chat)
+    # Init Claude AI
+    client = anthropic.Anthropic(
+        api_key=os.getenv('ANTHROPIC_API_KEY')
+    )
+
+    x_player = ClaudePlayer('X', client)
+    o_player = GeminiPlayer('O', chat)
     game = TicTacToe()
     play(game, x_player, o_player, print_game=True)
